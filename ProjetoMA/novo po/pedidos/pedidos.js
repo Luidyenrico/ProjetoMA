41e7ee0f961edb6bf9ae7f8ccc4ea63321324488
Luidy Enrico <80763934+Luidyenrico@users.noreply.github.com>
2023-07-06 23:35:15 -0300

    Main

---
+++ ProjetoMA/novo po/pedidos/pedidos.js
@@ -0,0 +1,6 @@
+tinymce.init({
+    selector: '#myTextarea',
+    height: 300,
+    plugins: 'advlist autolink lists link image charmap print preview anchor',
+    toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  });
